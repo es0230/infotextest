c733a315917ff9ad055a21a4ab43cba797b444b9
es0230 <caneyok@gmail.com>
2022-02-08 14:10:24 +0300

    created table header with object keys

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import users from './users.js'; // добавляем массив пользователей
 
+let div = document.createElement('div');				// создаём
+let table = document.createElement('table');		// элементы
+let tableHeader = document.createElement('tr');	// страницы
+
+document.body.append(div);	// добавляем
+div.append(table);					// созданные
+table.append(tableHeader);	// элементы
+
+for (let i = 0; i < Object.entries(users[0]).length; i++) { 
+	let th = document.createElement('th');
+	th.textContent = Object.entries(users[0])[i][0];
+	tableHeader.append(th);
+} // наполняем заголовок таблицы ключами объектов массива пользователей
